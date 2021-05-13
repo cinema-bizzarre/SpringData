52e9adb5c23fd46d48e51a957eb8c617dfe10cab
cinema-bizzarre <cinema-bizzarre@yandex.ru>
2021-05-13 16:30:52 +0300

    Дз к 10 уроку

--- src/main/java/repository/ProductRepository.java
+++ src/main/java/repository/ProductRepository.java
@@ -1,12 +1,2 @@
-package repository;
-
-import module.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository {
-     Page<Product> findAllByPriceBetween(int min, int max, PageRequest of);
+package repository;public interface ProductRepository {
 }
--- src/main/java/services/ProductService.java
+++ src/main/java/services/ProductService.java
@@ -1,21 +1,2 @@
-package services;
-
-import module.Product;
-import org.springframework.data.domain.Page;
-
-import java.util.List;
-import java.util.Optional;
-
-
-public interface ProductService {
-
-    void save(Product product);
-    void deleteById(Long id);
-    Optional<Product> findOneById(Long id);
-    Page<Product> findAllByPriceBetween(int min, int max, int page, int size);
-    void incrementPriceById(Long id, int amount);
-    void decrementPriceById(Long id, int amount);
-    List<Product> findAll();
+package services;public class ProductService {
 }
-
-
